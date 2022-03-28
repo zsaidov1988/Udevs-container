4f4f54620f0805d0a8a15a706387d035e8f9d569
zsaidov1988 <zsaidov1988@gmail.com>
2022-03-28 17:51:20 +0500

    Initial

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => res.send("<h1>Hello World!</h1><p>DockerFile</p>"));
+app.get("/products", (req, res) => {
+  res.send([
+    {
+      productId: "101",
+      price: 100,
+    },
+    {
+      productId: "102",
+      price: 200,
+    },
+  ]);
+});
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
